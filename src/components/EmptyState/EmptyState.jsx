a56afa350662c3fcd98190a12c2b790db931fc39
shompaaa <shompaakter334@gmail.com>
2026-04-02 11:45:16 +0600

    all done & project published in surge

--- src/components/EmptyState/EmptyState.jsx
+++ src/components/EmptyState/EmptyState.jsx
@@ -10,7 +10,7 @@ const EmptyState = ({message, address, label}) => {
         to={address}
         className='relative inline-block px-4 py-2 font-medium group'
       >
-        <span className='absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0'></span>
+        <span className='absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:translate-x-0 group-hover:translate-y-0'></span>
         <span className='absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary'></span>
         <span className='relative text-black group-hover:text-white'>
           {label}
